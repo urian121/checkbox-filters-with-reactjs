81a559dabf18f3d1de87dd226465ea5f2009aec6
URIAN VIERA <urian1213viera@gmail.com>
2024-07-13 15:18:15 -0500

    iniciando desarrollo

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Filter from "./components/Filter";
+import "./App.css";
+import "./style/home.css";
+import "./style/cards.css";
+
+function App() {
+  return <Filter />;
+}
+
+export default App;
